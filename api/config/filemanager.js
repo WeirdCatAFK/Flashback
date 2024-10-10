f7dfe5599b7ad538d84501d3ffa3e0855c4c9e23
Daniel Pineda Torres <dpineda.dev@gmail.com>
2024-10-10 09:46:57 -0600

    Reworked most  filemanager functions

--- api/config/filemanager.js
+++ api/config/filemanager.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const path = require("path");
+const { promisify } = require("util");
 
 class FileManager {
   constructor() {
@@ -14,43 +15,135 @@ class FileManager {
       throw new Error("Current workspace not found in config.");
     }
 
-    // Check if the path is absolute, if not resolve it to the project directory
     this.filesPath = path.isAbsolute(currentWorkspace.path)
       ? currentWorkspace.path
-      : // Resolve relative paths within the project's directory on the context of flashback as a whole, not on the api
-        path.resolve(__dirname, "../../", currentWorkspace.path);
+      : path.resolve(__dirname, "../../", currentWorkspace.path);
   }
 
-  async ensureDirectoryExists() {
+  async getCurrentFilePath() {
+    return this.filesPath;
+  }
+
+  async getFileTree(directoryPath = this.filesPath) {
+    try {
+      const stats = fs.statSync(directoryPath);
+      const name = path.basename(directoryPath);
+
+      const item = {
+        name: name,
+        type: stats.isDirectory() ? "folder" : path.extname(name).substring(1),
+      };
+      if (stats.isDirectory()) {
+        item.children = fs
+          .readdirSync(directoryPath)
+          .map((child) => this.getFileTree(path.join(directoryPath, child)));
+      }
+
+      return item;
+    } catch (error) {
+      console.error(`Error getting file tree: ${error.message}`);
+      throw error;
+    }
+  }
+
+  async getFileStats(fileName) {
+    try {
+      const filePath = path.join(this.filesPath, fileName);
+      return fs.statSync(filePath);
+    } catch (error) {
+      console.error(`Error getting file stats: ${error.message}`);
+      throw error;
+    }
+  }
+
+  async listFiles(directoryPath = this.filesPath) {
+    try {
+      return fs.readdirSync(directoryPath);
+    } catch (error) {
+      console.error(`Error listing files: ${error.message}`);
+      throw error;
+    }
+  }
+
+  async searchFiles(pattern, directoryPath = this.filesPath) {
+    try {
+      const files = await this.listFiles(directoryPath);
+      return files.filter((file) => file.match(new RegExp(pattern)));
+    } catch (error) {
+      console.error(`Error searching files: ${error.message}`);
+      throw error;
+    }
+  }
+
+  async ensureDir() {
     if (!fs.existsSync(this.filesPath)) {
       fs.mkdirSync(this.filesPath, { recursive: true });
       console.log("Created workspace files directory:", this.filesPath);
     }
   }
 
-  loadDocument(fileName) {
-    const filePath = path.join(this.filesPath, fileName);
-    if (!fs.existsSync(filePath)) {
-      throw new Error(
-        `File ${fileName} not found in workspace with path ${filePath}`
-      );
+  async saveFile(fileName, content) {
+    try {
+      const filePath = path.join(this.filesPath, fileName);
+      fs.writeFileSync(filePath, content, "utf8");
+      console.log(`File ${fileName} saved in workspace.`);
+    } catch (error) {
+      console.error(`Error saving file: ${error.message}`);
+      throw error;
     }
-    return fs.readFileSync(filePath, "utf8");
   }
 
-  saveDocument(fileName, content) {
-    const filePath = path.join(this.filesPath, fileName);
-    fs.writeFileSync(filePath, content, "utf8");
-    console.log(`File ${fileName} saved in workspace.`);
+  async deleteFile(fileName) {
+    try {
+      const filePath = path.join(this.filesPath, fileName);
+      if (fs.existsSync(filePath)) {
+        fs.unlinkSync(filePath);
+        console.log(`File ${fileName} deleted from workspace.`);
+      } else {
+        throw new Error(`File ${fileName} not found.`);
+      }
+    } catch (error) {
+      console.error(`Error deleting file: ${error.message}`);
+      throw error;
+    }
   }
 
-  deleteDocument(fileName) {
-    const filePath = path.join(this.filesPath, fileName);
-    if (fs.existsSync(filePath)) {
-      fs.unlinkSync(filePath);
-      console.log(`File ${fileName} deleted from workspace.`);
-    } else {
-      throw new Error(`File ${fileName} not found.`);
+  async loadFile(fileName) {
+    try {
+      const filePath = path.join(this.filesPath, fileName);
+      if (!fs.existsSync(filePath)) {
+        throw new Error(
+          `File ${fileName} not found in workspace with path ${filePath}`
+        );
+      }
+      return fs.readFileSync(filePath, "utf8");
+    } catch (error) {
+      console.error(`Error loading file: ${error.message}`);
+      throw error;
+    }
+  }
+
+  async copyFile(sourceFileName, destinationFileName) {
+    try {
+      const sourcePath = path.join(this.filesPath, sourceFileName);
+      const destinationPath = path.join(this.filesPath, destinationFileName);
+      fs.copyFileSync(sourcePath, destinationPath);
+      console.log(`File ${sourceFileName} copied to ${destinationFileName}.`);
+    } catch (error) {
+      console.error(`Error copying file: ${error.message}`);
+      throw error;
+    }
+  }
+
+  async moveFile(sourceFileName, destinationFileName) {
+    try {
+      const sourcePath = path.join(this.filesPath, sourceFileName);
+      const destinationPath = path.join(this.filesPath, destinationFileName);
+      fs.renameSync(sourcePath, destinationPath);
+      console.log(`File ${sourceFileName} moved to ${destinationFileName}.`);
+    } catch (error) {
+      console.error(`Error moving file: ${error.message}`);
+      throw error;
     }
   }
 }
