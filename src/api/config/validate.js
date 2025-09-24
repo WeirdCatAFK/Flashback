68ce6e58d8f830e004bcda23469132972de4a157
Daniel <pinedadpt@gmail.com>
2025-09-24 11:37:13 -0600

    Fixed validator logic, since it was faulty

--- src/api/config/validate.js
+++ src/api/config/validate.js
@@ -1,17 +1,10 @@
 // Module that returns a true value if the config is ready to use, using the validator modules
-import validateEnv from './validators/env.js';
 import validateConfig from './validators/config.js';
 import validateDatabase from './validators/database.js';
 function validate() {
-    const env = validateEnv();
-    if (env) {
-        if (validateConfig({ env: env })) return true
-        if (validateDatabase()) return true
-        return false
-    } else {
-        console.log("Couldn't identify the environment on validation");
-        return false
-    }
+    if (validateConfig() && validateDatabase()) return true
+
+    return false
 
 }
 
